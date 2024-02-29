92f30521d1e68dee3f459f53c3f9fe60329ee806
Asiyanaveed <160754902+Asiyanaveed@users.noreply.github.com>
2024-02-29 17:35:25 +0500

    Add files via upload

---
+++ 10th.ts
@@ -0,0 +1,5 @@
+//excersicse 10
+//my name is Asiya
+//Date : 29/02/2024
+//This program will be run simple code hello  world
+console.log(`hello world simple program`);
---
+++ 2ndprogram.ts
@@ -0,0 +1,5 @@
+//excersice 10
+//My name is Asiya
+//Date 29/02/2024
+//This program will multiplicaation 
+console.log(5*2);
